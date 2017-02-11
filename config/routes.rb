0c47ec833fdbb0a517ec1a12302638f9e49385a5
BrunoBoehm <bruno.boehm@gmail.com>
2017-02-11 13:50:07 +0100

    with the bootrstrap generator

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :companies
   get 'static_pages/dashboard'
 
-  get 'static_pages/frontpage'
+  root 'static_pages#frontpage'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
