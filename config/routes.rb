2b2f1eb1ab5cca913449848e6640355ffd85a364
BrunoBoehm <bruno.boehm@gmail.com>
2017-02-11 20:43:39 +0100

    patched registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   if Rails.env.production?
-    devise_for :users, controllers: { sessions: 'users/registrations' }
+    devise_for :users, controllers: { registrations: 'users/registrations' }
   else
     devise_for :users
   end
